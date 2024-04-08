d1b61767af139e543d247047b69d8dcee0c90744
246994 <246994@vutbr.cz>
2024-04-08 09:44:24 +0200

    samsukol1

--- searching.py
+++ searching.py
@@ -23,9 +23,22 @@ def read_data(file_name, field):
     with open(file_name,"r") as f:
         data = json.load(f)
     return data.get(field)
+def binary_search(numbers,target):
+    left, right = 0, len(numbers) - 1
 
+    while left <= right:
+        mid = (left + right) // 2
+        if numbers[mid] == target:
+            return mid
+        elif numbers[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return None
 def main():
-    pass
+    sequentional_data = read_data("sequential.json","unordered_numbers")
+    print(sequentional_data)
 
 
 if __name__ == '__main__':
