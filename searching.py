65f7d663def274fef1d2ffff909fd3f9d1571bcd
246994 <246994@vutbr.cz>
2025-04-07 08:59:32 +0200

    2025 ukol1

--- searching.py
+++ searching.py
@@ -13,16 +13,23 @@ def read_data(file_name, field):
     :return: (list, string),
     """
     file_path = os.path.join(cwd_path, file_name)
-
-    with open("sequential.json","r") as f:
-        allowed_key = json.load(f)
-
-    if field not in allowed_key:
+    if field not in {"unordered_numbers","ordered_numbers","dna_sequence"}:
         return None
 
-    with open(file_name,"r") as f:
-        data = json.load(f)
-    return data.get(field)
+    with open(file_path,"r") as json_file:
+        seq = json.load(json_file)
+    return seq[field]
+    # 2024
+    # with open("sequential.json","r") as f:
+    #     allowed_key = json.load(f)
+
+    # 2024
+    # if field not in allowed_key:
+    #     return None
+    #
+    # with open(file_name,"r") as f:
+    #     data = json.load(f)
+    # return data.get(field)
 
 
 def linear_search(sequence, target):
@@ -38,15 +45,15 @@ def linear_search(sequence, target):
         - 'positions': A list of positions (indices) where the target is found.
         - 'count': The count of occurrences of the target in the sequence.
     """
-    positions = []
-    count = 0
-
-    for i, num in enumerate(sequence):
-        if num == target:
-            positions.append(i)
-            count += 1
-
-    return {'positions': positions, 'count': count}
+    # positions = []
+    # count = 0
+    #
+    # for i, num in enumerate(sequence):
+    #     if num == target:
+    #         positions.append(i)
+    #         count += 1
+    #
+    # return {'positions': positions, 'count': count}
 
 
 def pattern_search(sequence, pattern):
@@ -60,32 +67,38 @@ def pattern_search(sequence, pattern):
     Returns:
     - set: A set containing the positions (indices) where the pattern is found in the sequence.
     """
-    positions = set()
-    seq_length = len(sequence)
-    pattern_length = len(pattern)
+    # positions = set()
+    # seq_length = len(sequence)
+    # pattern_length = len(pattern)
+    #
+    # for i in range(seq_length - pattern_length + 1):
+    #     if sequence[i:i + pattern_length] == pattern:
+    #         positions.add(i)
+    #
+    # return positions
 
-    for i in range(seq_length - pattern_length + 1):
-        if sequence[i:i + pattern_length] == pattern:
-            positions.add(i)
-
-    return positions
-
-def binary_search(numbers,target):
-    left, right = 0, len(numbers) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-        if numbers[mid] == target:
-            return mid
-        elif numbers[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return None
+# def binary_search(numbers,target):
+#     left, right = 0, len(numbers) - 1
+#
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if numbers[mid] == target:
+#             return mid
+#         elif numbers[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#
+#     return None
 def main():
-    sequentional_data = read_data("sequential.json","unordered_numbers")
-    print(sequentional_data)
+    file_name = "sequential.json"
+
+    sequential_data = read_data(file_name, field="unordered_numbers")
+    print(sequential_data)
+
+    # 2024
+    # sequentional_data = read_data("sequential.json","unordered_numbers")
+    # print(sequentional_data)
 
 
 if __name__ == '__main__':
